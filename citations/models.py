38c7ea9a7de7e4f6cddcdd9f69a564b218460b14
Rafael JPD <rafael.pezzuto@gmail.com>
2022-06-29 16:11:26 -0300

    Cria estrutura básica de nova app citations

---
+++ citations/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+from wagtail.admin.edit_handlers import FieldPanel
---
+++ citations/wagtail_hooks.py
@@ -0,0 +1,3 @@
+from django.utils.translation import gettext as _
+
+from wagtail.contrib.modeladmin.options import (ModelAdmin, modeladmin_register)
