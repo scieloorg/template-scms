ce87124d669e9b3e008ea478bb18f8fe84859d15
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-09-05 19:50:57 -0300

    Adiciona as classes para os formulários de carga / descarga de dados de thematic_areas

--- thematic_areas/wagtail_hooks.py
+++ thematic_areas/wagtail_hooks.py
@@ -1,13 +1,30 @@
+from django.urls import include, path
 from django.http import HttpResponseRedirect
 from django.utils.translation import gettext as _
 
-from wagtail.contrib.modeladmin.views import CreateView
+from wagtail.core import hooks
+from wagtail.contrib.modeladmin.views import CreateView, EditView
 from wagtail.contrib.modeladmin.options import (ModelAdmin, modeladmin_register, ModelAdminGroup)
 
-from .models import ThematicArea
+from .models import ThematicArea, ThematicAreaFile
+from .button_helper import ThematicAreaHelper
+
+
+class ThematicAreaEditView(EditView):
+
+    def form_valid(self, form):
+        self.object = form.save_all(self.request.user)
+        return HttpResponseRedirect(self.get_success_url())
 
 
 class ThematicAreaCreateView(CreateView):
+
+    def form_valid(self, form):
+        self.object = form.save_all(self.request.user)
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class ThematicAreaFileCreateView(CreateView):
 
     def form_valid(self, form):
         self.object = form.save_all(self.request.user)
@@ -30,4 +47,35 @@ class ThematicAreaAdmin(ModelAdmin):
     export_filename = 'thematic_areas'
 
 
-modeladmin_register(ThematicAreaAdmin)
+class ThematicAreaFileAdmin(ModelAdmin):
+    model = ThematicAreaFile
+    ordering = ('-updated',)
+    create_view_class=ThematicAreaFileCreateView
+    button_helper_class = ThematicAreaHelper
+    menu_label = _('Thematic Areas Upload')
+    menu_icon = 'folder'
+    menu_order = 200
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ('attachment', 'line_count', 'is_valid', 'creator',
+                    'updated', 'created', )
+    list_filter = ('is_valid', )
+    search_fields = ('attachment', )
+
+
+class ThematicAreaAdminGroup(ModelAdminGroup):
+    menu_label = _('Thematic Areas')
+    menu_icon = 'folder-open-inverse'
+    menu_order = 200
+    items = (ThematicAreaAdmin, ThematicAreaFileAdmin,)
+
+
+modeladmin_register(ThematicAreaAdminGroup)
+
+
+@hooks.register('register_admin_urls')
+def register_url():
+    return [
+        path('thematic_areas/ThematicAreafile/',
+        include('thematic_areas.urls', namespace='thematic_areas')),
+    ]
