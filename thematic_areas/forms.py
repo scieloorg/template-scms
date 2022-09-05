b9c79e29bfd4decc5e0419ac7a5f232a2296378f
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-09-05 19:46:03 -0300

    Cria o modelo do formulário para subir arquivo com dados de thematic_areas

--- thematic_areas/forms.py
+++ thematic_areas/forms.py
@@ -14,3 +14,18 @@ class ThematicAreaForm(WagtailAdminModelForm):
         self.save()
 
         return thematic
+
+
+class ThematicAreaFileForm(WagtailAdminModelForm):
+
+    def save_all(self, user):
+        thematic_areas_file = super().save(commit=False)
+
+        if self.instance.pk is not None:
+            thematic_areas_file.updated_by = user
+        else:
+            thematic_areas_file.creator = user
+
+        self.save()
+
+        return thematic_areas_file
