d331a9e8ad5bf5a833e2e68a33d136f88ac56f65
Jamil Atta Junior <atta.jamil@innolabs.com.br>
2022-10-16 08:04:36 -0300

    Remove javascript desnecessário

--- core/static/js/custom.js
+++ core/static/js/custom.js
@@ -1,7 +1,1 @@
 /* your custom js go here */
-
-$('.lang-select').change(function(){
-    let url = $(this).find(':selected').attr('data-url');
-    $('#language').val($(this).val());
-    $('#form_lang').submit();
-});
